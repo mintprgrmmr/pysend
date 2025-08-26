1a3dd604640b5277fa10fef961858e96f4080192
Mint <mintprgrmmr@gmail.com>
2025-08-26 16:54:37 +0300

    feat: черновой рабочий вариант tcp-клиента

--- src/client.py
+++ src/client.py
@@ -1,4 +1,29 @@
 import socket
+import errno
+
+HOST = "127.0.0.1"
+PORT = 1234
 
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+try:
+    clientsocket.connect((HOST, PORT))
+    print(f"Клиент установил соединение с {HOST}:{PORT}")
+
+    message = "Hello server"
+    clientsocket.send (message.encode())
+    
+    data = clientsocket.recv (1024)
+    reply = data.decode()
+    print("Ответ сервера:", reply)
+
+    clientsocket.close()
+
+except ConnectionRefusedError as er:
+    if er.errno == errno.ECONNREFUSED:
+        print(f"В подключении отказано: сервер не слушает порт. Клиент завершил работу")
+        exit(1)
+    else:
+        print("Ошибка подключения:", er)
+except Exception as er:
+    print(f"Неожиданная ошибка при подключении к серверу: {er}")
