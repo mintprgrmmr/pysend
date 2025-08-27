fec37bd7267cdee86754b58fbc3901e1a005348d
Mint <mintprgrmmr@gmail.com>
2025-08-28 01:08:23 +0300

    feat: реализован обмен файлом между tcp клиентом и сервером, скорректирован вывод программ

--- src/client.py
+++ src/client.py
@@ -3,27 +3,48 @@ import errno
 
 HOST = "127.0.0.1"
 PORT = 1234
+SIZE = 1024
 
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
     clientsocket.connect((HOST, PORT))
-    print(f"[+] Клиент установил соединение с {HOST}:{PORT}.")
+    print(f"[CONNECTING]Клиент установил соединение с {HOST}:{PORT}.")
 
-    message = "Hello server!"
-    clientsocket.send (message.encode())
-    
-    data = clientsocket.recv (1024)
-    reply = data.decode()
-    print("[+] Ответ сервера:", reply)
+    filename = "sendfile.txt"
+    print(f"[REQUESTING] Имя файла для отправки: {filename}")
+    clientsocket.send((filename + "\n").encode())
+
+    file = open("data/sendfile.txt", "rb")
+    print("[OPEN]Файл открыт для чтения в бинарном режиме.\n[SENDING] Отправка содержимого файла...")
+    while True:
+        chunk = file.read(SIZE)
+        if not chunk:
+            break
+        clientsocket.sendall(chunk)
+    file.close()
+
+    clientsocket.shutdown(socket.SHUT_WR)
+    print("[SENDING] Отправка завершена.")
+
+    message = clientsocket.recv(SIZE).decode()
+    print(f"[SERVER] Ответ:{message}")
 
     clientsocket.close()
 
 except ConnectionRefusedError as er:
     if er.errno == errno.ECONNREFUSED:
-        print(f"[-] В подключении отказано: сервер не слушает порт. Клиент завершил работу.")
+        print("[ConnectionRefusedError]В подключении отказано: сервер не слушает порт. Клиент завершил работу.")
         exit(1)
     else:
-        print("[-] Ошибка подключения:", er)
+        print("[ConnectionRefusedError]Ошибка подключения:", er)
+except OSError as er:
+    if er.errno == errno.EISCONN:
+        print(f"[OSError]Сокет уже подключен: {er}. Клиент завершил работу.")
+        exit(1)
+    else:
+        print(f"[OSError] Ошибка №{er.errno}: {er.strerror}")
+except FileNotFoundError:
+    print(f"[FileNotFoundError] Не найден файл {filename}. Проверь путь/имя.")
 except Exception as er:
-    print(f"[-] Неожиданная ошибка при подключении к серверу: {er}")
+    print(f"[ERROR]Неожиданная ошибка при подключении к серверу: {er}")
--- src/server.py
+++ src/server.py
@@ -3,36 +3,53 @@ import errno
 
 HOST = "127.0.0.1"
 PORT = 1234
+SIZE = 1024
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print ("[+] Сокет успешно создан.")
+print ("[BOOT]Сокет успешно создан.")
 
 try:
     serversocket.bind((HOST, PORT))
-    print(f"[+] Cервер запущен на {HOST}:{PORT}.")
+    print(f"[STARTING]Cервер запущен на {HOST}:{PORT}.")
 
     serversocket.listen(5)
-    print("[+] Ожидание подключений...")
+    print("[LISTENING]Ожидание подключений...")
 
 except OSError as er:
     if er.errno == errno.EADDRINUSE:
-        print(f"[-] Порт {PORT} уже занят. Завершение работы\nСервер завершил работу.")
+        print(f"[OSError]Порт {PORT} уже занят. Завершение работы\nСервер завершил работу.")
         exit(1)
 except Exception as er:
-    print(f"[-] Неожиданная ошибка при запуске сервера: {er}")
+    print(f"[ERROR]Неожиданная ошибка при запуске сервера: {er}")
 
 while True:
     clientsocket, clientaddr = serversocket.accept()
-    print("[+] Подключился клиент:", clientaddr)
+    print("[CLIENT]Подключился клиент:", clientaddr)
 
-    data = clientsocket.recv (1024)
-    if not data:
-        print("[-] Клиент разорвал соединение.")
-        break
-    message = data.decode()
-    print(f"[+] Клиент отправил: {message}")
-    message = "-> Hello client!" \
-    
-    clientsocket.send (message.encode())
+    filenamebytes = clientsocket.recv(SIZE)
+    print("[RECV]Получены данные о файле.")
+    if not filenamebytes: 
+        print("[CLIENT]Соединение разорвано.")
+        clientsocket.close()
+        continue
+
+    filename = filenamebytes.decode().strip()
+    print(f"[CLIENT]Имя файла: {filename}.\n[RECV] Приём содержимого файла...")
+
+    fileout = open(filename, "wb")
+    print("[OPEN]Файл открыт для перезаписи в бинарном режиме.")
+    while True:
+        chunk = clientsocket.recv(SIZE)
+        if not chunk:
+            break
+        fileout.write(chunk)
+    fileout.close()
+    print("[FINISHED] Файл успешно сохранён.")     
+
+    try:
+        clientsocket.send(b"THANKS FOR FILE BRO!!!")
+        print("[ACK] Подтверждение отправлено клиенту.")
+    except Exception as er:
+        print(f"[ERROR]Неожиданная ошибка при получении файла: {er}")
 
     clientsocket.close()
