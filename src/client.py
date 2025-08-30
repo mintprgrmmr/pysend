7accda2ea032962d20c3449944b8c245dd15cfee
Mint <mintprgrmmr@gmail.com>
2025-08-30 19:04:56 +0300

    feat: доведенный до рабочего состояния клиент, исключен ненужный код

--- src/client.py
+++ src/client.py
@@ -1,5 +1,4 @@
 import socket
-import errno
 
 HOST: str = "127.0.0.1"
 PORT: int = 1235
@@ -7,6 +6,7 @@ SIZE: int = 1024
 SOURCE_DIR: str = "test/data/source"
 SOURCE_NAME: str = "source.bin"
 SEND_NAME: str = "sendfile.txt"
+STOP_MARK: bytes = b"__STOP__"
 
 def run_client() -> None:
     """
@@ -15,7 +15,36 @@ def run_client() -> None:
     """
     clientsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+    try:
+        clientsocket.connect((HOST, PORT))
+        print(f"[CONNECTING]Клиент установил соединение с {HOST}:{PORT}.")
 
+        print(f"[REQUESTING] Имя файла для отправки: {SEND_NAME}")
+        clientsocket.send(SEND_NAME.encode())
+
+        file = open(SOURCE_DIR + "/" + SOURCE_NAME, "rb")
+        print("[OPEN]Файл открыт для чтения в бинарном режиме.\n[SENDING] Отправка содержимого файла...")
+        while True:
+            chunk: bytes = file.read(SIZE)
+            if not chunk:
+                break
+            clientsocket.send(chunk)
+        file.close()
+
+        clientsocket.send(STOP_MARK)
+        print("[SENDING] Отправка завершена.")
+
+        message: str = clientsocket.recv(SIZE).decode()
+        print(f"[SERVER] Ответ:{message}")
+
+        clientsocket.close()
+
+    except FileNotFoundError:
+            print(f"[FileNotFoundError] Не найден файл {SOURCE_NAME}. Проверь путь/имя.")
+    except OSError as er:
+            print(f"[OSError] Ошибка №{er.errno}: {er.strerror}")
+    except Exception as er:
+        print(f"[ERROR]Неожиданная ошибка при подключении к серверу: {er}")
 
 def main() -> None:
     """
