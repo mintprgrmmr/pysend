74f4ab32937e11c5b984838632ce6aea9789a6d5
Mint <mintprgrmmr@gmail.com>
2025-08-30 13:55:43 +0300

    feat: добавлены функции в клиент и сервер, аналогичные tcp

--- src/client.py
+++ src/client.py
@@ -2,8 +2,26 @@ import socket
 import errno
 
 HOST: str = "127.0.0.1"
-PORT: int = 1234
+PORT: int = 1235
 SIZE: int = 1024
 SOURCE_DIR: str = "test/data/source"
 SOURCE_NAME: str = "source.bin"
 SEND_NAME: str = "sendfile.txt"
+
+def run_client() -> None:
+    """
+    Простой UDP-клиент: подключается к серверу, отправляет имя файла
+    и его содержимое в байтах, затем ждёт подтверждение и закрывает соединение.
+    """
+    clientsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+
+def main() -> None:
+    """
+    Точка входа: запускает клиента.
+    """
+    run_client()
+
+if __name__ == "__main__":
+    main()
--- src/server.py
+++ src/server.py
@@ -2,6 +2,24 @@ import socket
 import errno
 
 HOST: str = "127.0.0.1"
-PORT: int = 1234
+PORT: int = 1235
 SIZE: int = 1024
 SAVE_DIR: str = "test/data/saved"
+
+def run_server() -> None:
+    """
+    Простой UDP-сервер: принимает имя файла и содержимое,
+    сохраняет его в SAVE_DIR и отправляет подтверждение клиенту.
+    """
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+
+def main():
+    """
+    Точка входа: запускает сервер.
+    """
+    run_server()
+
+if __name__ == "__main__":
+    main()
